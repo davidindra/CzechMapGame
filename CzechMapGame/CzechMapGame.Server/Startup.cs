ad7d8ae5ddeb614841141222869ae6934c02645c
Jiří Zapletal <mail@jzapletal.cz>
2019-09-27 12:50:32 +0200

    Added legend + other updates

--- CzechMapGame/CzechMapGame.Server/Startup.cs
+++ CzechMapGame/CzechMapGame.Server/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json.Serialization;
 using System.Linq;
 using Microsoft.Extensions.FileProviders;
 
